a9af040544c7155e3c8ce9db33c06477941e9915
kiramux <kiramux39@gmail.com>
2025-10-07 16:03:09 +0800

    add unit tests for entries CRUD

---
+++ db/sqlc/entry_test.go
@@ -0,0 +1,73 @@
+package db
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/kiramux/simpleBank/util"
+	"github.com/stretchr/testify/require"
+)
+
+// 传入一个 account ，这个 account 在调用次函数前创建，因为需要在此 account 下创建 entries
+func createRandomEntry(t *testing.T, account Account) Entry {
+	arg := CreateEntryParams{
+		AccountID: account.ID,
+		Amount:    util.RandomInt(-100, 100),
+	}
+	entry, err := testQueries.CreateEntry(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry)
+
+	require.Equal(t, arg.AccountID, entry.AccountID)
+	require.Equal(t, arg.Amount, entry.Amount)
+
+	require.NotZero(t, entry.ID)
+	require.NotZero(t, entry.CreatedAt)
+
+	return entry
+}
+
+func TestCreateEntry(t *testing.T) {
+	account := createRandomAccount(t)
+	createRandomEntry(t, account)
+}
+
+func TestGetEntry(t *testing.T) {
+	account := createRandomAccount(t)
+	entry1 := createRandomEntry(t, account)
+
+	entry2, err := testQueries.GetEntry(context.Background(), entry1.ID)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, entry2)
+
+	require.Equal(t, entry1.ID, entry2.ID)
+	require.Equal(t, entry1.AccountID, entry2.AccountID)
+	require.Equal(t, entry1.Amount, entry2.Amount)
+	require.WithinDuration(t, entry1.CreatedAt, entry2.CreatedAt, time.Second)
+
+}
+
+func TestListEntries(t *testing.T) {
+	account := createRandomAccount(t)
+	for range 10 {
+		createRandomEntry(t, account)
+	}
+
+	arg := ListEntriesParams{
+		AccountID: account.ID,
+		Limit:     5,
+		Offset:    5,
+	}
+	entries, err := testQueries.ListEntries(context.Background(), arg)
+
+	require.NoError(t, err)
+	require.Len(t, entries, 5)
+
+	for _, entry := range entries {
+		require.NotEmpty(t, entry)
+		require.Equal(t, arg.AccountID, entry.AccountID)
+	}
+
+}
